b64957453eb391a0f081eac9418e2bd3f1d5c1a8
hannahklauber <hannahklauber@posteo.net>
2020-08-03 17:40:05 +0200

    first commit

---
+++ code/figure_3.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b070153feeef8c269ddcc185f30b44da79c8b56b55cacd035df485bd3d64c301
+size 11736
---
+++ code/twostage_bootstrap_batches.do
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8ee87673e8a035495b0d1f607b84417ebc4966f400a7584bd59361e0ffb8709
+size 7293
